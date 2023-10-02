f39d2e55136c91e5f0c706bc7216d69601dc0d36
100levelz <j100levelz@gmail.com>
2023-10-02 19:57:57 +0000

    added user model

--- App/models/user.py
+++ App/models/user.py
@@ -1,37 +1,37 @@
-from werkzeug.security import check_password_hash, generate_password_hash
-from flask_login import UserMixin
-from App.database import db
+    from werkzeug.security import check_password_hash, generate_password_hash
+    from flask_login import UserMixin
+    from App.database import db
 
-class User(db.Model, UserMixin):
-    userID = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String(100), nullable=False, unique=True)
-    fName = db.Column(db.String(100), nullable=False)
-    lName = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
-    
+    class User(db.Model, UserMixin):
+        userID = db.Column(db.Integer, primary_key=True)
+        username =  db.Column(db.String(100), nullable=False, unique=True)
+        fName = db.Column(db.String(100), nullable=False)
+        lName = db.Column(db.String(100), nullable=False)
+        password = db.Column(db.String(120), nullable=False)
+        email = db.Column(db.String(100), nullable=False)
+        
 
-    def __init__(self, fName, lName, email, username, password):
-        self.fName = fName
-        self.lName = lName
-        self.email = email
-        self.username = username
-        self.set_password(password)
+        def __init__(self, fName, lName, email, username, password):
+            self.fName = fName
+            self.lName = lName
+            self.email = email
+            self.username = username
+            self.set_password(password)
 
-    def get_json(self):
-        return{
-            'id': self.id,
-            'fName': self.fName,
-            'lName': self.lName,
-            'email': self.email,
-            'username': self.username
-        }
+        def get_json(self):
+            return{
+                'id': self.id,
+                'fName': self.fName,
+                'lName': self.lName,
+                'email': self.email,
+                'username': self.username
+            }
 
-    def set_password(self, password):
-        """Create hashed password."""
-        self.password = generate_password_hash(password, method='sha256')
-    
-    def check_password(self, password):
-        """Check hashed password."""
-        return check_password_hash(self.password, password)
+        def set_password(self, password):
+            """Create hashed password."""
+            self.password = generate_password_hash(password, method='sha256')
+        
+        def check_password(self, password):
+            """Check hashed password."""
+            return check_password_hash(self.password, password)
 
