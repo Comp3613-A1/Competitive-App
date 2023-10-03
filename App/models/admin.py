04591cceb4fb919361760eba2a38898161e2764e
Khadisha <123189196+khadishaclarke@users.noreply.github.com>
2023-10-03 23:51:36 +0000

    added admin model and method1

---
+++ App/models/admin.py
@@ -0,0 +1,25 @@
+from App.models import *
+from App.database import db
+
+class Admin(db.Model):
+    __tablename__ = 'Admin'
+    staffID = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, unique=False)
+   
+    def __init__(self, staffID, user_id):
+        self.staffID = staffID
+        self.user_id = user_id
+
+    def create_competition(self, competition_name, start_date, end_date, division, description):
+        # Create a new competition and add it to the database
+        competition = Competition(
+            staffID=self.staffID,
+            name=competition_name,
+            startDate=start_date,
+            endDate=end_date,
+            division=division,
+            description=description  # Add description field
+        )
+        db.session.add(competition)
+        db.session.commit()
+        return competition
