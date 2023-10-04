c56f215a0a5648a34c8ff17aff480251db60a87a
100levelz <94690530+100levelz@users.noreply.github.com>
2023-10-03 20:25:50 -0400

    Added view comp details method to student.py

--- App/models/student.py
+++ App/models/student.py
@@ -9,4 +9,17 @@ class Student(db.Model):
     def __init__(self, studentID, user_id):
         self.studentID = studentID
         self.user_id = user_id
-        
+    
+    def view_competition_details(self, competition_identifier):
+        # Assuming you have a relationship between Student and Competition models,
+        # you can access the competitions associated with this student.
+
+        # Check if the competition_identifier is an integer (ID)
+        if isinstance(competition_identifier, int):
+            # Query by competition ID
+            competition = Competition.query.filter_by(id=competition_identifier, student_id=self.studentID).first()
+        else:
+            # Query by competition name 
+            competition = Competition.query.filter_by(name=competition_identifier, student_id=self.studentID).first()
+
+        return competition
