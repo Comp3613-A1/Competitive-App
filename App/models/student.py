3432bb7d9233f5d09a068d607d26fea9f1c106f4
Khadisha <123189196+khadishaclarke@users.noreply.github.com>
2023-10-03 21:23:39 -0400

    Updated student.py

--- App/models/student.py
+++ App/models/student.py
@@ -4,19 +4,19 @@ from App.database import db
 class Student(db.Model):
     __tablename__ = 'Student'
     studentID = db.Column(db.Integer,  primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False, unique = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.userID'), nullable = False, unique = False)
     
     def __init__(self, studentID, user_id):
         self.studentID = studentID
         self.user_id = user_id
     
     def view_competition_details(self, competition_identifier):
-        # Check if the competition_identifier is an integer (ID)
+        #Check if the competition_identifier is an integer (ID)
         if isinstance(competition_identifier, int):
-            # Query by competition ID
+            #Query by competition ID
             competition = Competition.query.filter_by(id=competition_identifier, student_id=self.studentID).first()
         else:
-            # Query by competition name 
+            #Query by competition name 
             competition = Competition.query.filter_by(name=competition_identifier, student_id=self.studentID).first()
 
         return competition
