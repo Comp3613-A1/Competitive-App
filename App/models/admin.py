e0c46824ab3470dc66817f7a2d21f0475f4e0f38
Khadisha <123189196+khadishaclarke@users.noreply.github.com>
2023-10-03 21:08:08 -0400

    Update admin.py

--- App/models/admin.py
+++ App/models/admin.py
@@ -4,7 +4,7 @@ from App.database import db
 class Admin(db.Model):
     __tablename__ = 'Admin'
     staffID = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.userID'), nullable=False, unique=False)
    
     def __init__(self, staffID, user_id):
         self.staffID = staffID
