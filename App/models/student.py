8824223d53bc501c270617109fb267bae478a96f
Khadisha <123189196+khadishaclarke@users.noreply.github.com>
2023-10-05 12:53:35 -0400

    Updated student model to inherit functionality of User

--- App/models/student.py
+++ App/models/student.py
@@ -1,7 +1,7 @@
 from App.models import *
 from App.database import db
 
-class Student(db.Model):
+class Student(User):
     __tablename__ = 'Student'
     studentID = db.Column(db.Integer,  primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.userID'), nullable = False, unique = False)
