4b9f68aa66f39eac3d107cc9596e57661bc226be
Faith Rose <70714890+F-rose@users.noreply.github.com>
2023-10-04 22:57:21 -0400

    Add files via upload

--- App/views/index.py
+++ App/views/index.py
@@ -8,6 +8,22 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 def index_page():
     return render_template('index.html')
 
+@index_views.route('/adminlogin', methods=['GET'])
+def admin_login_page():
+    return render_template('adminlogin.html')
+
+@index_views.route('/adminsignup', methods=['GET'])
+def admin_signup_page():
+    return render_template('adminsignup.html')
+
+@index_views.route('/studentlogin', methods=['GET'])
+def student_login_page():
+    return render_template('studentlogin.html')
+
+@index_views.route('/studentsignup', methods=['GET'])
+def student_signup_page():
+    return render_template('studentsignup.html')
+
 @index_views.route('/init', methods=['GET'])
 def init():
     db.drop_all()
