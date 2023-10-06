395521ad46d0b3b30e62e42ea76e4823adb8e9c4
Khadisha <123189196+khadishaclarke@users.noreply.github.com>
2023-10-06 19:56:30 -0400

    Update student.py

--- App/models/student.py
+++ App/models/student.py
@@ -4,13 +4,13 @@ from App.database import db
 class Student(User):
     __tablename__ = 'Student'
     studentID = db.Column(db.Integer,  primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.userID'), nullable = False, unique = False)
+    userID = db.Column(db.Integer, db.ForeignKey('user.userID'), nullable = False, unique = False)
     score = db.Column(db.Float, nullable = False, unique = False, default=0)
     notification = db.Column(db.String(500), nullable = False, unique = False, default = 'No Notifications.')
     
-    def __init__(self, studentID, user_id, score=0, notification='No Notifications.'):
+    def __init__(self, studentID, userID, score=0, notification='No Notifications.'):
         self.studentID = studentID
-        self.user_id = user_id
+        self.userID = userID
         self.score = score
         self.notification = notification
    
