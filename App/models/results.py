7cd4aa86032334f9cd3d54388ebdad97381ba11a
Khadisha <123189196+khadishaclarke@users.noreply.github.com>
2023-10-06 19:55:34 -0400

    Update results.py

--- App/models/results.py
+++ App/models/results.py
@@ -3,14 +3,14 @@ from App.database import db
 
 class Results(db.Model):
     __tablename__ = 'Results'
-    results_id = db.Column(db.Integer, primary_key=True)
-    competition_id = db.Column(db.Integer, db.ForeignKey('competition.competitionID'), nullable=False)
-    student_id = db.Column(db.Integer, db.ForeignKey('student.studentID'), nullable=False)
+    resultsID = db.Column(db.Integer, primary_key=True)
+    competitionID = db.Column(db.Integer, db.ForeignKey('competition.competitionID'), nullable=False)
+    studentID = db.Column(db.Integer, db.ForeignKey('student.studentID'), nullable=False)
     position = db.Column(db.String(100), nullable=False)
     score = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, competition_id, student_id, position, score):
-        self.competition_id = competition_id
-        self.student_id = student_id
+    def __init__(self, competitionID, studentID, position, score):
+        self.competitionID = competitionID
+        self.studentID = studentID
         self.position = position
         self.score = score
