6ddbc54066095b0e6759b0870575590c873e46be
Khadisha <123189196+khadishaclarke@users.noreply.github.com>
2023-10-10 14:55:31 -0400

    Update student.py constructor again

--- App/models/student.py
+++ App/models/student.py
@@ -8,9 +8,8 @@ class Student(User):
     score = db.Column(db.Float, nullable = False, unique = False, default=0)
     notification = db.Column(db.String(500), nullable = False, unique = False, default = 'No Notifications.')
     
-    def __init__(self, userID, score=0, notification='No Notifications.', fName, lName, email, username, password):
+    def __init__(self, score=0, notification='No Notifications.', fName, lName, email, username, password):
         super().__init__(fName, lName, email, username, password)
-        self.userID = userID
         self.score = score
         self.notification = notification
    
