57874a91cf1cf98fdc2aa6056f5a07e712777466
Faith Rose <70714890+F-rose@users.noreply.github.com>
2023-10-21 17:25:32 -0400

    Update admin.py
    edited add_results func

--- App/controllers/admin.py
+++ App/controllers/admin.py
@@ -1,4 +1,5 @@
 from App.models import Competition
+from App.models import Results
 from sqlalchemy import func, desc
 from App.models import User
 from App.database import db
@@ -9,10 +10,13 @@ def create_admin(fName, lName, email, username, password):
     db.session.commit()
     return new_admin
 
-def create_competition(self, competition_name, start_date, end_date, division, description):
+def get_all_comp():
+    return Competition.query.all()
+
+def create_competition( competition_name, start_date, end_date, division, description):
     # Create a new competition and add it to the database
     competition = Competition(
-        staffID=self.staffID, #associates staff member with the competition they created
+        #staffID=self.staffID,self, #associates staff member with the competition they created
         name=competition_name,
         startDate=start_date,
         endDate=end_date,
@@ -23,26 +27,28 @@ def create_competition(self, competition_name, start_date, end_date, division, d
     db.session.commit()
     return competition
 
-def add_result(competition_id, student_id, position, score):
+def add_result(competitionID, studentID, position, score):
         
         # Store rankings before new results are added
-        prev_ranks = view_rankings()
+        #prev_ranks = view_rankings()
   
         # Create a new competition result and add it to the database
-        result = Result(
-            competition_id=competition_id,
-            student_id=student_id,
+        result = Results(
+            competitionID=competitionID,
+            studentID=studentID,
             position=position,
             score=score
         )
         db.session.add(result)
         db.session.commit()
+
+        return result
       
         # Get the rankings after results were added
-        current_ranks = view_rankings()
+        #current_ranks = view_rankings()
 
         # Find users who were in the top 20 in the previous rankings
-        top_20_previous = [user for user, position in prev_ranks[:20]]
+        """top_20_previous = [user for user, position in prev_ranks[:20]]
 
         # Find users who are in the top 20 in the current leaderboard
         top_20_current = [user for user, position in current_ranks[:20]]
@@ -52,8 +58,8 @@ def add_result(competition_id, student_id, position, score):
             if user not in top_20_current:
                 # User was in the top 20 previously but not anymore, send a notification
                 send_notification(user, "Your ranking has dropped out of the top 20.")
-
-        return result
+"""
+        #return result
 
 def send_notification(student, notif):
     student.add_notification(notif)
