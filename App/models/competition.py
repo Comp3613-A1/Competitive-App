e7b5db85fbdeaf67d25b610715de755aa6e3a8e6
Faith Rose <70714890+F-rose@users.noreply.github.com>
2023-10-22 16:29:52 -0400

    Update competition.py
    added get_json function

--- App/models/competition.py
+++ App/models/competition.py
@@ -1,20 +1,29 @@
-from App.models import *
 from App.database import db
 
 class Competition(db.Model):
-    __tablename__ = 'Competition'
+    #__tablename__ = 'Competition'
     competitionID = db.Column(db.Integer, primary_key=True)
-    staffID = db.Column(db.Integer, db.ForeignKey('Admin.staffID'), nullable=False, unique=False)
+    #staffID = db.Column(db.Integer, db.ForeignKey('Admin.staffID'), nullable=False, unique=False)
     name = db.Column(db.String(120), nullable=False)
     startDate = db.Column(db.String, unique=False, nullable=False)
     endDate = db.Column(db.String, unique=False, nullable=False)
     division = db.Column(db.String(50), unique=False, nullable=False)
     description = db.Column(db.String(255))  # Add a description field
 
-    def __init__(self, staffID, name, startDate, endDate, division, description):
-        self.staffID = staffID
+    def __init__(self, name, startDate, endDate, division, description):
+        #self.staffID = staffID
         self.name = name
         self.startDate = startDate
         self.endDate = endDate
         self.division = division
         self.description = description
+
+    def get_json(self):
+        return{
+            'id': self.competitionID,
+            'name': self.name,
+            'Start Date': self.startDate,
+            'End Date': self.endDate,
+            'Division': self.division,
+            'Description': self.description
+        }
