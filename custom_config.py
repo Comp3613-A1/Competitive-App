226b8416d219a2362bd85e95061a111c8dfba601
Faith Rose <70714890+F-rose@users.noreply.github.com>
2023-10-26 18:58:52 -0400

    Update custom_config.py

--- custom_config.py
+++ custom_config.py
@@ -1,3 +1,5 @@
+from sqlalchemy.dialects.postgresql import psycopg2
+from flask_sqlalchemy import SQLAlchemy
 SQLALCHEMY_DATABASE_URI = "postgres://rank_my_coding_db_user:ib7eeE4XJ6Y0xCXNrcd3cfocafzsMRSG@dpg-ckia7cke1qns73bomdt0-a/rank_my_coding_db"
 SECRET_KEY = "green grapes"
 JWT_ACCESS_TOKEN_EXPIRES = 7
