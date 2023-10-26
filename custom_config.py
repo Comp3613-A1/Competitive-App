f10291840ddaf324eddd9454e3e6e6f83a269173
Faith Rose <70714890+F-rose@users.noreply.github.com>
2023-10-26 19:40:32 -0400

    Update custom_config.py

--- custom_config.py
+++ custom_config.py
@@ -1,6 +1,4 @@
-from sqlalchemy.dialects.postgresql import psycopg2
-from flask_sqlalchemy import SQLAlchemy
-SQLALCHEMY_DATABASE_URI = "postgres://rank_my_coding_db_user:ib7eeE4XJ6Y0xCXNrcd3cfocafzsMRSG@dpg-ckia7cke1qns73bomdt0-a/rank_my_coding_db"
+SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
 SECRET_KEY = "green grapes"
 JWT_ACCESS_TOKEN_EXPIRES = 7
-ENV = "PRODUCTION"
+#ENV = "PRODUCTION"
