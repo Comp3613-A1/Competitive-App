21f97c92b87e953b63ddb069cd909678d7cad83e
Faith Rose <70714890+F-rose@users.noreply.github.com>
2023-10-26 18:54:23 -0400

    Update custom_config.py

--- custom_config.py
+++ custom_config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgres://rank_my_coding_db_user:ib7eeE4XJ6Y0xCXNrcd3cfocafzsMRSG@dpg-ckia7cke1qns73bomdt0-a/rank_my_coding_db"
 SECRET_KEY = "green grapes"
 JWT_ACCESS_TOKEN_EXPIRES = 7
-ENV = "DEVELOPMENT"
+ENV = "PRODUCTION"
