401cbe9d39835a3a96ac4987eb9a4b2e0850bfbc
Khadisha <123189196+khadishaclarke@users.noreply.github.com>
2023-10-27 22:01:48 +0000

    update

--- wsgi.py
+++ wsgi.py
@@ -6,12 +6,9 @@ from App.database import db, get_migrate
 from App.main import create_app
 from App.controllers import ( create_user, get_all_users_json, get_all_users )
 
-# This commands file allow you to create convenient CLI commands for testing controllers
-
 app = create_app()
 migrate = get_migrate(app)
 
-# This command creates and initializes the database
 @app.cli.command("init", help="Creates and initializes the database")
 def initialize():
     db.drop_all()
@@ -22,22 +19,14 @@ def initialize():
 '''
 User Commands
 '''
-
-# Commands can be organized using groups
-
-# create a group, it would be the first argument of the comand
-# eg : flask user <command>
 user_cli = AppGroup('user', help='User object commands') 
 
-# Then define the command and any parameters and annotate it with the group (@)
 @user_cli.command("create", help="Creates a user")
 @click.argument("username", default="rob")
 @click.argument("password", default="robpass")
 def create_user_command(username, password):
     create_user(username, password)
     print(f'{username} created!')
-
-# this command will be : flask user create bob bobpass
 
 @user_cli.command("list", help="Lists users in the database")
 @click.argument("format", default="string")
@@ -47,7 +36,7 @@ def list_user_command(format):
     else:
         print(get_all_users_json())
 
-app.cli.add_command(user_cli) # add the group to the cli
+app.cli.add_command(user_cli) 
 
 '''
 Test Commands
